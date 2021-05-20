d17f334b144feb1b45efd9aac586dce053e525b6
Rikocar <richard.mrvar@gmail.com>
2021-05-20 22:45:14 +0200

    Added the foundation of item drops and block item drops.

---
+++ Minecraft - Copy/Assets/Scripts/Items/Item Drops/CreateBlockMesh.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateBlockMesh : MonoBehaviour
+{
+	[SerializeField] private Material _blockMaterial;
+
+    private void Start()
+    {
+		var gameObject = new GameObject("Block Prefab", typeof(MeshFilter), typeof(MeshRenderer));
+		var meshFilter = gameObject.GetComponent<MeshFilter>();
+		var meshRenderer = gameObject.GetComponent<MeshRenderer>();
+
+		var mesh = new Mesh();
+		var verts = new List<Vector3>();
+		var normals = new List<Vector3>();
+		var UVs = new List<Vector3>();
+		var quads = new List<int>();
+
+		// Generate each face's info.
+		for (int i = 0; i < 6; ++i)
+		{
+			verts.AddRange(BlockData.vertices[i]);
+			normals.AddRange(BlockData.normals[i]);
+			UVs.AddRange(BlockData.UVs3[i]);
+			quads.AddRange(BlockData.quads[i]);
+		}
+
+		mesh.SetVertices(verts);
+		mesh.SetNormals(normals);
+		mesh.SetUVs(0, UVs);
+		mesh.SetIndices(quads.ToArray(), MeshTopology.Quads, 0);
+
+		meshFilter.sharedMesh = mesh;
+		meshRenderer.materials = new Material[] { _blockMaterial };
+
+		PrefabUtility.SaveAsPrefabAsset(gameObject, "Assets/Block Prefab.prefab", out bool success);
+		if (success)
+		{
+			Debug.Log("Successfully made block prefab!");
+		}
+		else
+		{
+			Debug.LogError("Failed to make block prefab!");
+		}
+	}
+}
---
+++ Minecraft - Copy/Assets/Scripts/Items/Item Drops/ItemDropAnimController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropAnimController : MonoBehaviour
+{
+	[SerializeField] private Transform _graphicsRoot;
+
+	[SerializeField] private float _rotationSpeed;
+	[SerializeField] private float _bobDelta;
+	[SerializeField] private float _bobSpeed;
+
+	private Vector3 _bobPointA;
+	private Vector3 _bobPointB;
+	private bool _bobSwitch;
+
+	private void Awake()
+	{
+	}
+
+	private void Update()
+	{
+		_bobPointA = new Vector3(0, +_bobDelta);
+		_bobPointB = new Vector3(0, -_bobDelta);
+		var dt = Time.deltaTime;
+
+		_graphicsRoot.transform.Rotate(0, _rotationSpeed * dt, 0);
+
+		var targetBobPoint = _bobSwitch ? _bobPointA : _bobPointB;
+		_graphicsRoot.localPosition = Vector3.Lerp(_graphicsRoot.localPosition, targetBobPoint, _bobSpeed * dt);
+		if ((targetBobPoint - _graphicsRoot.localPosition).sqrMagnitude <= 0.001F)
+		{
+			_bobSwitch = !_bobSwitch;
+		}
+	}
+}
